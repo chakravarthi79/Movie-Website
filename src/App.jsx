366b84814efeebd6e1ab74ad0e026ca30f6284fc
chakravarthi79 <kummarichakravarthi79@gmail.com>
2024-11-08 16:58:55 +0530

    routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         
 
         {/* <Route path={'/'}  element = {<Navigate to = {'/home'}/> } /> */}
-        <Route index path={'/home'} element ={<Home/>}/>
+        <Route  path={'/'} element ={<Home/>}/>
         <Route path={'/popular'} element ={<Popular/>}/>
         <Route path={'/toprated'} element ={<TopRated/>}/>
         <Route path={'/upcoming'} element ={<UpComing/>}/>
